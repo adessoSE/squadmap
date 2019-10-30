007b0f70dbffb2f12608e0c6eaa1b974441d2218
Cem Caylak <cem.caylak@adesso.de>
2019-10-30 09:27:33 +0100

    Basic setup of squadmap

---
+++ squadmap-backend/src/test/java/de/adesso/squadmap/SquadmapApplicationTests.java
@@ -0,0 +1,13 @@
+package de.adesso.squadmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquadmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
