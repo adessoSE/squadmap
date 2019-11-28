5fc44f34d6514f8a21e9bfbc8a0a0bb8a0cef8d3
Cem Caylak <cem.caylak@adesso.de>
2019-11-28 09:05:39 +0100

    Add basic employee and team

--- squadmap-backend/src/main/java/models/Employee.java
+++ squadmap-backend/src/main/java/models/Employee.java
@@ -1,4 +1,18 @@
 package models;
 
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
 public class Employee {
+    private long employeeId;
+    private String firstname;
+    private String lastname;
+    private LocalDate birthday;
+    private String email;
+    private String phone;
+    private boolean isExternal;
+    private List<Team> teams;
 }
--- squadmap-backend/src/main/java/models/Team.java
+++ squadmap-backend/src/main/java/models/Team.java
@@ -1,4 +1,13 @@
 package models;
 
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class Team {
+    private long teamId;
+    private String name;
+    private Employee teamleader;
+    private List<Employee> employees;
 }
