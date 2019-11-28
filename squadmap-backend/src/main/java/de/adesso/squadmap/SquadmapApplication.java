e968cfb20afbfb1bcd43b475d56bf10ca7237b6e
Cem Caylak <cem.caylak@adesso.de>
2019-11-28 12:13:20 +0100

    Add annotation and spring-boot-actuar

--- squadmap-backend/src/main/java/de/adesso/squadmap/SquadmapApplication.java
+++ squadmap-backend/src/main/java/de/adesso/squadmap/SquadmapApplication.java
@@ -2,8 +2,10 @@ package de.adesso.squadmap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 
 @SpringBootApplication
+@EnableNeo4jRepositories
 public class SquadmapApplication {
 
 	public static void main(String[] args) {
