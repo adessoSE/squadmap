f44c0f21c548748dbbf0780600e5a87abf3d858f
bliedung <alexander.bliedung@adesso.de>
2019-11-28 10:07:38 +0100

    added repository, service and controller for Project

---
+++ squadmap-backend/src/main/java/Controller/ProjectController.java
@@ -0,0 +1,42 @@
+package Controller;
+
+import Service.ProjectService;
+import models.Project;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController("/project")
+public class ProjectController {
+
+    private ProjectService projectService;
+
+    public ProjectController(ProjectService projectService){
+        this.projectService = projectService;
+    }
+
+    @GetMapping("")
+    public Iterable<Project> getAllProjects(){
+        return projectService.getAllProjects();
+    }
+
+    @GetMapping("/{projectId}")
+    public Optional<Project> getProjectById(@PathVariable long projectId){
+        return projectService.getProjectById(projectId);
+    }
+
+    @PostMapping("/create")
+    public Project createProject(Project project){
+        return projectService.saveProject(project);
+    }
+
+    @PutMapping("/update/{projectId}")
+    public Project updateProject(@PathVariable long projectId, Project project){
+        return projectService.saveProject(project);
+    }
+
+    @DeleteMapping("/delete/{projectId}")
+    public void deleteProject(@PathVariable long projectId){
+        projectService.deleteProjectById(projectId);
+    }
+}
---
+++ squadmap-backend/src/main/java/Service/ProjectService.java
@@ -0,0 +1,33 @@
+package Service;
+
+import Repository.ProjectRepository;
+import models.Project;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ProjectService {
+
+    private ProjectRepository projectRepository;
+
+    public ProjectService(ProjectRepository projectRepository){
+        this.projectRepository = projectRepository;
+    }
+
+    public Iterable<Project> getAllProjects(){
+        return projectRepository.findAll();
+    }
+
+    public Optional<Project> getProjectById(long id){
+        return projectRepository.findById(id);
+    }
+
+    public Project saveProject(Project project){
+        return projectRepository.save(project);
+    }
+
+    public void deleteProjectById(long projectId){
+        projectRepository.deleteById(projectId);
+    }
+}
