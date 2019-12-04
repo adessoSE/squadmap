d291b4bf944733e43afe0eb478cba4c507c0a4c0
bliedung <alexander.bliedung@adesso.de>
2019-12-04 15:58:12 +0100

    implemented project controller

--- squadmap-backend/src/main/java/de/adesso/squadmap/controller/ProjectController.java
+++ squadmap-backend/src/main/java/de/adesso/squadmap/controller/ProjectController.java
@@ -1,13 +1,16 @@
 package de.adesso.squadmap.controller;
 
+import de.adesso.squadmap.port.driver.project.create.CreateProjectCommand;
 import de.adesso.squadmap.port.driver.project.create.CreateProjectUseCase;
 import de.adesso.squadmap.port.driver.project.delete.DeleteProjectUseCase;
+import de.adesso.squadmap.port.driver.project.get.GetProjectResponse;
 import de.adesso.squadmap.port.driver.project.get.GetProjectUseCase;
 import de.adesso.squadmap.port.driver.project.get.ListProjectUseCase;
+import de.adesso.squadmap.port.driver.project.update.UpdateProjectCommand;
 import de.adesso.squadmap.port.driver.project.update.UpdateProjectUseCase;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -31,5 +34,30 @@ public class ProjectController {
         this.createProjectUseCase = createProjectUseCase;
         this.updateProjectUseCase = updateProjectUseCase;
         this.deleteProjectUseCase = deleteProjectUseCase;
+    }
+
+    @GetMapping("/all")
+    public List<GetProjectResponse> getAllProjects(){
+        return listProjectUseCase.listProjects();
+    }
+
+    @GetMapping("/{projectId}")
+    public GetProjectResponse getProject(@PathVariable long projectId){
+        return getProjectUseCase.getProject(projectId);
+    }
+
+    @PostMapping("/create")
+    public Long createProject(@RequestBody CreateProjectCommand command){
+        return createProjectUseCase.createProject(command);
+    }
+
+    @PutMapping("/update/{projectId}")
+    public void updateProject(@RequestBody UpdateProjectCommand command, @PathVariable long projectId){
+        updateProjectUseCase.updateProject(command, projectId);
+    }
+
+    @DeleteMapping("/delete/{projectId}")
+    public void deleteProject(@PathVariable long projectId){
+        deleteProjectUseCase.deleteProject(projectId);
     }
 }
