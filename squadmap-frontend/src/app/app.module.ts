241560c124e4175a53cfe667d39e8986876df44c
Cem Caylak <cem.caylak@adesso.de>
2019-12-11 13:09:39 +0100

    Add basic angular components and routing

--- squadmap-frontend/src/app/app.module.ts
+++ squadmap-frontend/src/app/app.module.ts
@@ -1,16 +1,48 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouterModule, Routes} from '@angular/router';
+import {AppComponent} from './app.component';
+import {NgModule} from '@angular/core';
+import {NavigationComponent} from './models/navigation/navigation.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {BsDropdownModule} from 'ngx-bootstrap/dropdown';
+import {CollapseModule} from 'ngx-bootstrap/collapse';
+import {ProjectComponent} from './models/project/project.component';
+import {EmployeeComponent} from './models/employee/employee.component';
+import {PageNotFoundComponent} from './models/employee/page-not-found/page-not-found.component';
 
-import { AppComponent } from './app.component';
+const routes: Routes = [
+  {
+    path: 'employee',
+    component: EmployeeComponent
+  },
+  {
+    path: 'project',
+    component: ProjectComponent
+  },
+
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    ProjectComponent,
+    EmployeeComponent,
+    PageNotFoundComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot(),
+    CollapseModule.forRoot(),
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
