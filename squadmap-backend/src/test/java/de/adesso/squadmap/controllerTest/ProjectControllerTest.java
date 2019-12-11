a1c74ccfb09b6fd2ec2ad1f23e095b397dea54c4
bliedung <alexander.bliedung@adesso.de>
2019-12-11 13:22:52 +0100

    tests for workingOn services

--- squadmap-backend/src/test/java/de/adesso/squadmap/controllerTest/ProjectControllerTest.java
+++ squadmap-backend/src/test/java/de/adesso/squadmap/controllerTest/ProjectControllerTest.java
@@ -5,8 +5,10 @@ import de.adesso.squadmap.service.project.*;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class ProjectControllerTest {
 
     @Mock
