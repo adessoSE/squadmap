85826f3f801881bf55913b1c36e3d926c8ddb15e
emir-gs <bursali_emir@hotmail.de>
2019-12-16 19:22:46 +0100

    added EmployeeForm in navigation

--- squadmap-frontend/src/app/navigation/navigation.component.ts
+++ squadmap-frontend/src/app/navigation/navigation.component.ts
@@ -1,8 +1,8 @@
 import {Component, OnInit, TemplateRef} from '@angular/core';
 import {BsDropdownConfig, BsModalRef, BsModalService} from 'ngx-bootstrap';
-import {EmployeeModel} from '../models/employee.model';
-import {ProjectModel} from '../models/project.model';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {FormBuilder, FormGroup, NgForm} from '@angular/forms';
+import {EmployeeService} from '../service/employee.service';
+import {CreateEmployeeModel} from '../models/createEmployee.model';
 
 @Component({
   selector: 'app-navigation',
@@ -11,13 +11,14 @@ import {FormBuilder, FormGroup} from '@angular/forms';
   providers: [{provide: BsDropdownConfig, useValue: {isAnimated: true, autoClose: true}}]
 })
 export class NavigationComponent implements OnInit {
+
   isCollapsed = true;
   modalRef: BsModalRef;
   employeeForm: FormGroup;
 
-  constructor(private modalService: BsModalService, private formbuilder: FormBuilder) {
-    this.createEmployeeForm();
-  }
+  constructor(private modalService: BsModalService,
+              private formbuilder: FormBuilder,
+              private employeeService: EmployeeService) { this.createEmployeeForm(); }
 
   private createEmployeeForm() {
     this.employeeForm = this.formbuilder.group({
@@ -40,4 +41,20 @@ export class NavigationComponent implements OnInit {
   ngOnInit() {
   }
 
+  onCreateEmployee(employeeForm: NgForm) {
+    const employee = new CreateEmployeeModel(
+      employeeForm.value.firstName,
+      employeeForm.value.lastName,
+      employeeForm.value.birthday,
+      employeeForm.value.email,
+      employeeForm.value.phone,
+      employeeForm.value.isExternal
+    );
+    this.employeeService.addEmployee(employee).subscribe(
+      res => {
+        console.log(res);
+      }
+    );
+    this.modalRef.hide();
+  }
 }
