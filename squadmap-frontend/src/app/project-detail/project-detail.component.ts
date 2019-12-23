220c67d8132ba261a9746763ec2b7615b1f6689e
emir-gs <bursali_emir@hotmail.de>
2019-12-23 13:42:27 +0100

    added add employee functionality to project detail view

--- squadmap-frontend/src/app/project-detail/project-detail.component.ts
+++ squadmap-frontend/src/app/project-detail/project-detail.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, TemplateRef} from '@angular/core';
 import {ProjectModel} from '../models/project.model';
 import {ActivatedRoute} from '@angular/router';
 import {ProjectService} from '../service/project.service';
@@ -6,6 +6,7 @@ import {EmployeeService} from '../service/employee.service';
 import {EmployeeModel} from '../models/employee.model';
 import {WorkingOnService} from '../service/workingOn.service';
 import {WorkingOnEmployeeModel} from '../models/workingOnEmployee.model';
+import {BsModalRef, BsModalService} from 'ngx-bootstrap';
 
 @Component({
   selector: 'app-project-detail',
@@ -18,18 +19,20 @@ export class ProjectDetailComponent implements OnInit {
   private filteredEmployees: EmployeeModel[];
   searchText: string;
   isSearching: boolean;
+  modalRef: BsModalRef;
 
   constructor(private route: ActivatedRoute,
               private projectService: ProjectService,
               private employeeService: EmployeeService,
-              private workingOnService: WorkingOnService) { }
+              private workingOnService: WorkingOnService,
+              private modalService: BsModalService) { }
 
   ngOnInit() {
     this.isSearching = false;
-    this.project = new ProjectModel(0,  '', '', null, null, false, []);
+    this.project = new ProjectModel(0,  '', '', null, null, false, []); // vermeidet exceptions beim Aufbau der view
     this.refreshProject();
+    this.searchText = '';
   }
-
 
   onSearchForEmployee() {
     if (this.searchText.length === 0) {
@@ -42,14 +45,20 @@ export class ProjectDetailComponent implements OnInit {
   }
 
   onAddEmployee(employee: EmployeeModel) {
-    this.workingOnService.addEmployeeToProject(employee, this.project).subscribe(() => {
-      this.project.employees.push(new WorkingOnEmployeeModel( 0, employee, new Date(), new Date() ));
+    this.workingOnService.addEmployeeToProject(employee, this.project).subscribe(workingOnId => {
+      this.project.employees.push(new WorkingOnEmployeeModel( +workingOnId, employee, new Date(), new Date() ));
       this.isSearching = false;
+      this.modalRef.hide();
     });
+  }
 
+  onAddEmployeeModal(addEmployeeModal: TemplateRef<any>) {
+    this.filteredEmployees = this.employeeService.getCurrentEmployees();
+    this.modalRef = this.modalService.show(addEmployeeModal);
   }
 
   onDelete(workingOn: WorkingOnEmployeeModel) {
+    console.log(workingOn.workingOnId);
     this.workingOnService.removeEmployeeFromProject(workingOn.workingOnId).subscribe(() => {
         this.refreshProject();
     });
