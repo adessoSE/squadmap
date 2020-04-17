0023d01f3673c6e279c50f1364ebb8270d705150
Emir Gülec <emir.guelec@adesso.de>
2020-04-17 11:49:36 +0200

    changed validator not to accept dates after year 2500

--- squadmap-frontend/src/app/validators/minimum-date-validator.ts
+++ squadmap-frontend/src/app/validators/minimum-date-validator.ts
@@ -2,7 +2,7 @@ import {AbstractControl} from "@angular/forms";
 
 export function minimumDateValidator(control: AbstractControl): {[key: string]: any} | null {
   const date: Date = new Date(control.value);
-  if(date.getFullYear()<1900){
+  if(date.getFullYear()<1900 || date.getFullYear()>2500){
     return {'minimumDate':true};
   }else{
     return null;
